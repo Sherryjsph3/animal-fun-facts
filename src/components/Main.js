bdba9cc7e64f5aa918bdc7611b7daf46a8827690
Sherry Joseph <sherryjoseph@sherrys-air.myfiosgateway.com>
2021-09-14 20:30:10 -0400

    adds components and title

---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+function Main() {
+    const title = ""
+    const animalFacts = title === "" ? "Click an animal for a fun fact" : title
+    return (
+        <h1>{animalFacts}</h1>
+    )
+}
+
+export default Main;
